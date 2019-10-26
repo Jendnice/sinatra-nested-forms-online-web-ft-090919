d3be4f74ad0b15ce900d775b2e8d4627e44a4170
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-26 15:42:08 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,21 +1,44 @@
+# class Ship
+  
+#   attr_reader :name, :type, :booty
+#   @@ships = []
+  
+#   def initialize(params)
+#     @name = name 
+#     @type = type 
+#     @booty = booty 
+#     @@ships << self 
+#   end 
+  
+#   def self.all 
+#     @@ships
+#   end 
+  
+#   def self.clear
+#     @@ships.clear 
+#   end 
+  
+# end
+
+  
 class Ship
-  
-  attr_reader :name, :type, :booty
+  attr_accessor :name, :type, :booty
+
   @@ships = []
-  
+
   def initialize(params)
-    @name = name 
-    @type = type 
-    @booty = booty 
-    @@ships << self 
-  end 
-  
-  def self.all 
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@ships << self
+  end
+
+  def self.all
     @@ships
-  end 
-  
+  end
+
   def self.clear
-    @@ships.clear 
-  end 
-  
+    @@ships = []
+  end
+
 end
