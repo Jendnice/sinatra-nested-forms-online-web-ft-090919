6ba742b63ddb60020c7525ecd91382c20bb30e6f
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-26 15:43:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,55 +1,55 @@
-# require './environment'
-
-# module FormsLab
-#   class App < Sinatra::Base
-
-#     get '/' do 
-#       erb :root
-#     end 
-    
-#     get '/new' do 
-#       erb :'pirates/new'
-#     end 
-    
-#     post '/pirates' do 
-#       @pirate = Pirate.new(params[:pirate])
-      
-#       params[:pirate][:ships].each do |details|
-#         Ship.new(details)
-#       end 
-      
-#       @ships = Ship.all
-      
-#       erb :'pirates/show'
-#     end 
-
-#   end
-# end
-
-
 require './environment'
 
 module FormsLab
   class App < Sinatra::Base
 
-    get '/' do
+    get '/' do 
       erb :root
-    end
-
-    get '/new' do
+    end 
+    
+    get '/new' do 
       erb :'pirates/new'
-    end
-
-    post '/pirates' do
+    end 
+    
+    post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
-
+      
       params[:pirate][:ships].each do |details|
         Ship.new(details)
-      end
+      end 
+      
       @ships = Ship.all
-
+      
       erb :'pirates/show'
-    end
+    end 
 
   end
 end
+
+
+# require './environment'
+
+# module FormsLab
+#   class App < Sinatra::Base
+
+#     get '/' do
+#       erb :root
+#     end
+
+#     get '/new' do
+#       erb :'pirates/new'
+#     end
+
+#     post '/pirates' do
+#       @pirate = Pirate.new(params[:pirate])
+
+#       params[:pirate][:ships].each do |details|
+#         Ship.new(details)
+#       end
+#       @ships = Ship.all
+
+#       erb :'pirates/show'
+#     end
+
+#   end
+# end
