3e663541454caaad9f0b14828f939d5bdecd14dd
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-26 15:42:25 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,17 +1,36 @@
+# class Pirate
+  
+#   attr_reader :name, :weight, :height
+#   PIRATES = []
+  
+#   def initialize(params)
+#     @name = params[:name]
+#     @weight = params[:weight]
+#     @height = params[:height]
+#     PIRATES << self 
+#   end 
+  
+#   def self.all 
+#     PIRATES
+#   end 
+  
+# end
+
 class Pirate
-  
-  attr_reader :name, :weight, :height
+
+  attr_accessor :name, :weight, :height
+
   PIRATES = []
-  
+
   def initialize(params)
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    PIRATES << self 
-  end 
-  
-  def self.all 
+    PIRATES << self
+  end
+
+  def self.all
     PIRATES
-  end 
-  
+  end
+
 end
