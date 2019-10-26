a363c119f729894f56b8496ca7d3fc9f03baa267
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-26 15:43:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,3 +26,30 @@
 #   end
 # end
 
+
+require './environment'
+
+module FormsLab
+  class App < Sinatra::Base
+
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
+  end
+end
