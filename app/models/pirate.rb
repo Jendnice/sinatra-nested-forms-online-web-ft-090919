194be9f2f63bc38423a975a9b20ea529bed1e6da
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-26 15:51:42 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -16,21 +16,3 @@ class Pirate
   
 end
 
-# class Pirate
-
-#   attr_accessor :name, :weight, :height
-
-#   PIRATES = []
-
-#   def initialize(params)
-#     @name = params[:name]
-#     @weight = params[:weight]
-#     @height = params[:height]
-#     PIRATES << self
-#   end
-
-#   def self.all
-#     PIRATES
-#   end
-
-# end
